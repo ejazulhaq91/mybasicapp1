388d08a551381a000fe10d2b36f1b10d7e7e918b
ejazulhaq91 <57368961+ejazulhaq91@users.noreply.github.com>
2020-07-03 23:18:02 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World from Ejaz Ul Haq</h1>
+      <h2>This is my first react app</h2>
     </div>
   );
 }
